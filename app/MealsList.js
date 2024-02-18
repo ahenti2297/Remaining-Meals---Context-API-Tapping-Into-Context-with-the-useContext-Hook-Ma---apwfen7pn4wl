04442f4a6c8d6ff858f73151dbe5eeb136828b56
NEHA TIWARI <127203133+ahenti2297@users.noreply.github.com>
2024-02-18 18:12:44 +0530

    MealsList.js

--- app/MealsList.js
+++ app/MealsList.js
@@ -1,22 +1,27 @@
-"use client";
+'use client'
 import React, { useContext } from "react";
 import { MealsContext } from "./MealsProvider";
 
 const MealsList = () => {
-  const { meals, tickMeal } = useContext(MealsContext);
-  return (
-    <div>
-      <h2>Meals:</h2>
-      <ul>
-        {meals.map((meal) => (
-          <li key={meal.id}>
-            <input onChange={tickMeal} type="checkbox" value={meal.id} />
-            {meal.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    const { meals, tickMeal } = useContext(MealsContext);
+
+    return (
+        <div>
+            <h2>Meals:</h2>
+            <ul>
+                {meals.map((meal) => (
+                    <li key={meal.id}>
+                        <input 
+                            type="checkbox"
+                            checked={meal.ticked || false}
+                            onChange={() => tickMeal(meal.id)}
+                        />
+                        {meal.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default MealsList;
