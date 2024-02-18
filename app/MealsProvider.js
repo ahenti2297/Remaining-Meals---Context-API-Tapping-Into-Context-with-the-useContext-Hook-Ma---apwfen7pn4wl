568f28bd4fea6bff2cd5473c32ff7208d79d068a
NEHA TIWARI <127203133+ahenti2297@users.noreply.github.com>
2024-02-18 18:13:57 +0530

    MealsProvider.js

--- app/MealsProvider.js
+++ app/MealsProvider.js
@@ -1,37 +1,29 @@
-"use client";
+'use client'
 import React, { createContext, useState } from "react";
 
-const todaysMeals = [
-  { id: 1, name: "Baked Beans" },
-  { id: 2, name: "Baked Sweet Potatoes" },
-  { id: 3, name: "Baked Potatoes" },
-];
-
 export const MealsContext = createContext();
+const todaysMeals = [
+    { id: 1, name: 'Baked Beans' },
+    { id: 2, name: 'Baked Sweet Potatoes' },
+    { id: 3, name: 'Baked Potatoes' },
+]
 
 const MealsProvider = ({ children }) => {
-  const [meals, setMeals] = useState(todaysMeals);
+    const [meals, setMeals] = useState(todaysMeals);
 
-  const tickMeal = (e) => {
-    const mealId = Number(e.target.value);
-    setMeals((meals) => {
-      return meals.map((meal) => {
-        if (meal.id === mealId) {
-          return {
-            ...meal,
-            ticked: !meal.ticked,
-          };
-        }
-        return meal
-      });
-    });
-  };
+    const tickMeal = (id) => {
+        setMeals(
+            meals.map((meal) => (
+                meal.id === id ? { ...meal, ticked: !meal.ticked } : meal
+            ))
+        );
+    }
 
-  return (
-    <MealsContext.Provider value={{ meals, tickMeal }}>
-      {children}
-    </MealsContext.Provider>
-  );
+    return (
+        <MealsContext.Provider value={{ meals, tickMeal }}>
+            {children}
+        </MealsContext.Provider>
+    );
 };
 
 export default MealsProvider;
